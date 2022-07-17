5aef98eb0fe3fc021624b6b5bf52a8229b8b0354
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2022-07-17 09:46:58 -0300

    Initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by FABRICIO ALVARENGA on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
